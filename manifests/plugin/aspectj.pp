fda26ebbeb2c9bf7732cf3cae3d7efe6dcb920fb
Jerome Bridge <jeromebridge@gmail.com>
2015-08-30 21:57:44 -0400

    Adding AspectJ Plugin

---
+++ manifests/plugin/aspectj.pp
@@ -0,0 +1,43 @@
+# Class: eclipse::plugin::aspectj
+#
+# This class installs the Eclipse AspectJ Plugins
+#
+# Sample Usage:
+#
+#  include eclipse::plugin::aspectj
+#
+class eclipse::plugin::osgi (
+  $method           = 'package',
+  $ensure           = present
+) {
+
+  include eclipse
+  include eclipse::params
+
+  $repository = 'http://download.eclipse.org/tools/ajdt/44/dev/update/'
+
+  eclipse::plugin { 'aspectj-compiler':
+    iu         => 'org.aspectj.feature.group',
+    method     => $method,
+    repository => "${repository},${eclipse::repository}"
+  }
+
+  eclipse::plugin { 'equinox-weaving-sdk':
+    iu         => 'org.eclipse.equinox.weaving.sdk.feature.group',
+    method     => $method,
+    repository => "${repository},${eclipse::repository}"
+  }
+  
+  eclipse::plugin { 'cross-ref-source':
+    iu         => 'org.eclipse.contribution.xref.source.feature.group',
+    method     => $method,
+    repository => "${repository},${eclipse::repository}"
+  }
+
+  eclipse::plugin { 'eclipse-weaving-service-source':
+    iu         => 'org.eclipse.contribution.weaving.source.feature.group',
+    method     => $method,
+    repository => "${repository},${eclipse::repository}"
+  }
+
+}
